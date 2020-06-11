752cab605214faf956f5003fccf58baae2d4c3e0
netcommander <shenghe.qi>
2020-06-11 11:43:48 +0800

    1.0.1 add : sort core

--- src/main/java/cn/qishenghe/developassistant/util/mathutil/SortUtil.java
+++ src/main/java/cn/qishenghe/developassistant/util/mathutil/SortUtil.java
@@ -1,20 +1,34 @@
 package cn.qishenghe.developassistant.util.mathutil;
 
-import java.util.List;
-
 public class SortUtil {
 
-    // 冒泡携排序
-    public static void bubbleSorting (List<String> list, String seq, int index) {
+    // 冒泡排序核心（flag : true[由小到大]     false[由大到小]）
+    public static double[] bubbleSortCoreSl (double[] nums, boolean flag) {
 
-        // 1|qishenghe|18
-        // 2|maowei|20
-        // 3|zhangwanli|22
-
-        String[] arr = list.toArray(new String[0]);
-
-
-
+        for (int i = 0; i < nums.length; i ++) {
+            for (int j = nums.length - 1; j > i; j --) {
+                if (flag) {
+                    if (nums[j] < nums[j - 1]) {
+                        // small to large
+                        // change
+                        double tmp = nums[j];
+                        nums[j] = nums[j - 1];
+                        nums[j - 1] = tmp;
+                    }
+                } else {
+                    if (nums[j] > nums[j - 1]) {
+                        // large to small
+                        // change
+                        double tmp = nums[j];
+                        nums[j] = nums[j - 1];
+                        nums[j - 1] = tmp;
+                    }
+                }
+            }
+        }
+        return nums;
     }
+
+
 
 }
